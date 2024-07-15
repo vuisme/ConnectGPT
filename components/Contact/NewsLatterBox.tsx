f7fb069d1b911a42875965549f320f1a8a7de4c5
cpanel10x <me@vutn.net>
2024-07-15 16:14:04 +0700

    Update NewsLatterBox.tsx

--- components/Contact/NewsLatterBox.tsx
+++ components/Contact/NewsLatterBox.tsx
@@ -33,34 +33,33 @@ const NewsLatterBox = () => {
     >
       <h3 className="mb-4 text-2xl font-bold leading-tight text-black dark:text-white
       ">
-      🚀 Be a winner in this AI war.<br />
-      🥇  Be the first in your industry to use ConnectGPT.
+      🚀 Sẵn sàng trải nghiệm những điều mới và tiên phong áp dụng công nghệ mới nhất để vượt qua mọi đối thủ!<br />
+      Khám phá các tính năng vượt trội của giải pháp chatbot của chúng tôi
       </h3>
       <p className="text-base font-medium leading-relaxed text-body-color">
-        Get massive discounts, early access, and special surprises in the future by becoming a
-        ConnectGPT early adopter.
+        Để lại thông tin của bạn, chúng tôi sẽ chủ động liên hệ ngay khi nhận được hoặc truy cập @taobaobots để trải nghiệm trực tiếp!
       </p>
       <p className="mb-11 border-b border-body-color border-opacity-25 pb-11 text-base font-medium leading-relaxed text-body-color dark:border-white dark:border-opacity-25"></p>
       <form onSubmit={handleFormSubmit}>
         <input
           type="text"
           name="name" required
-          placeholder="Enter your name"
+          placeholder="Điền tên của bạn"
           className="mb-4 w-full rounded-md border border-body-color border-opacity-10 py-3 px-6 text-base font-medium text-body-color placeholder-body-color outline-none focus:border-primary focus:border-opacity-100 focus-visible:shadow-none dark:border-white dark:border-opacity-10 dark:bg-[#242B51] focus:dark:border-opacity-50"
         />
         <input
           type="email"
           name="email" required
-          placeholder="Enter your email"
+          placeholder="Điền email hoặc số điện thoại của bạn"
           className="mb-4 w-full rounded-md border border-body-color border-opacity-10 py-3 px-6 text-base font-medium text-body-color placeholder-body-color outline-none focus:border-primary focus:border-opacity-100 focus-visible:shadow-none dark:border-white dark:border-opacity-10 dark:bg-[#242B51] focus:dark:border-opacity-50"
         />
         <input
           type="submit"
-          value="🔥 Add me to the waitlist"
+          value="🔥 Tôi Sẵn Sàng"
           className="duration-80 mb-4 w-full cursor-pointer rounded-md border border-transparent bg-primary py-3 px-6 text-center text-base font-medium text-white outline-none transition ease-in-out hover:bg-opacity-80 hover:shadow-signUp focus-visible:shadow-none"
         />
         <p className="text-center text-base font-medium leading-relaxed text-body-color">
-          We will not send spam. Your email will not be shared with any third party.
+          Đừng lo lắng, chúng tôi sẽ không spam và chia sẻ thông tin của bạn với bên thứ 3.
         </p>
       </form>
 
