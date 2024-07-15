5f7a25146f4bd0a91791a126bd7b6be5beccbae8
cpanel10x <me@vutn.net>
2024-07-15 16:01:34 +0700

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -1,9 +1,9 @@
 export default function Head() {
   return (
     <>
-      <title>Connect GPT : connect with your customers</title>
+      <title>AI Chatbot - Tăng Cường Hiệu Suất CSKH Cho Doanh Nghiệp Với Chatbot AI Đa Ngôn Ngữ</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Not just another custome AI chatbot" />
+      <meta name="description" content="Chúng tôi cung cấp giải pháp chatbot tiên tiến, tích hợp AI và hỗ trợ nhiều ngôn ngữ, giúp doanh nghiệp tối ưu hoá quy trình chăm sóc khách hàng và tăng cường hiệu quả kinh doanh." />
       <link rel="icon" href="/images/logo/logosaas.png" />
     </>
   );
